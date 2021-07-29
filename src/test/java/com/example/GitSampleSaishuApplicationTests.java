eb57874856f3c26defd58219e983687c34257e20
saishuhayato <hayato.saishu@rakus.partners.co.jp>
2021-07-29 11:04:32 +0900

    first commit

---
+++ src/test/java/com/example/GitSampleSaishuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSampleSaishuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
